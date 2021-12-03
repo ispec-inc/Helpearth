2a511ae846ba8743f2d8d798dc3a78368cc5731d
hiroya nakajima <hiroya.nakajima@ispec.tech>
2021-12-03 12:05:08 +0900

    [add] write action

---
+++ Helpearth/URLBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  URLBuilder.swift
+//  Helpearth
+//
+//  Created by 中嶋裕也 on 2021/12/02.
+//
+
+import Foundation
+
+
+class URLBuilder {
+    static func getURL() -> String {
+        let baseHelpearthURL = "https://helpearth.bubbleapps.io/version-test/view?"
+        let id = UUID().uuidString
+        let url = baseHelpearthURL + "?id=" + id
+        return url
+    }
+}
